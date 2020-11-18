d8cb56df807480e7c93f4a8040508ec99079c532
Christian Bonato <bonatoque@yahoo.com>
2020-11-18 14:30:01 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,13 @@
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 
+<script>
+  
+  $(document).ready(function(){
+  
+$('a').attr('target','_blank');
+  
+// force PDF Files to open in new window
+    $('a[href$=".pdf"]').attr('target', '_blank');
+  });
+  
+</script>
